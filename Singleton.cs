9add650e3960f9273d23c3a3a0a7d6b64477b478
pavankumarpotta <pavankumar.potta1998@gmail.com>
2023-05-06 11:41:07 -0400

    singleton

---
+++ Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Singleton
+{
+    private static Singleton instance;
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Singleton instance1 = Singleton.Instance;
+        Singleton instance2 = Singleton.Instance;
+        Console.WriteLine(instance1 == instance2); 
+        string a = Console.ReadLine(); 
+        Console.WriteLine(a);
+    }
+}
+
